6c095af510ede116fe1b7fc224f69f7254ac2a96
Harshkhatri86 <khatriharsh86@gmail.com>
2024-03-28 01:04:43 +0530

    MySQL Database setup using sequelize-cli

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import express from 'express' ; 
-import bodyParser from 'body-parser';
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "../models";
 
-dotenv.config()
+dotenv.config();
 
-const app = express() ; 
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(cors()); 
-app.use(express.json())
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
-app.get('/' , (req , res) =>{
-    res.json("Api is running")
-})
+app.get("/", (req, res) => {
+  res.json("Api is running");
+});
 
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
-const bindAdress = process.env.BIND_ADDRESS || '0.0.0.0'
-app.listen(port , bindAdress, () =>{
-    console.log(`Server is running at http://localhost:${port}`)
-})
+db.sequelize.sync().then(() => {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+  const bindAdress = process.env.BIND_ADDRESS || "0.0.0.0";
+  console.log("connected to Database")
+  app.listen(port, bindAdress, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+});
